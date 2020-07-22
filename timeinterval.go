a046b20f700a56833630dbf67a4de1589215f441
Vu Anh <vuanh1995@gmail.com>
2020-07-22 15:03:45 +0700

    Change type of TimeInterval's Start and End field

--- timeinterval.go
+++ timeinterval.go
@@ -1,16 +1,41 @@
 package gpass
 
-import "github.com/sonda2208/gpass/walletobjects"
+import (
+	"time"
+
+	"github.com/sonda2208/gpass/walletobjects"
+)
+
+type DateTime struct {
+	Date time.Time
+}
+
+func (dt *DateTime) toWO() *walletobjects.DateTime {
+	return &walletobjects.DateTime{
+		Date: dt.Date.UTC().Format(time.RFC3339),
+	}
+}
+
+func woToDateTime(dt *walletobjects.DateTime) *DateTime {
+	if dt == nil {
+		return nil
+	}
+
+	res, _ := time.Parse(time.RFC3339, dt.Date)
+	return &DateTime{
+		Date: res,
+	}
+}
 
 type TimeInterval struct {
-	Start *walletobjects.DateTime
-	End   *walletobjects.DateTime
+	Start *DateTime
+	End   *DateTime
 }
 
 func (ti *TimeInterval) toWO() *walletobjects.TimeInterval {
 	return &walletobjects.TimeInterval{
-		Start: ti.Start,
-		End:   ti.End,
+		Start: ti.Start.toWO(),
+		End:   ti.End.toWO(),
 		Kind:  "walletobjects#timeInterval",
 	}
 }
@@ -21,7 +46,7 @@ func woToTimeInterval(ti *walletobjects.TimeInterval) *TimeInterval {
 	}
 
 	return &TimeInterval{
-		Start: ti.Start,
-		End:   ti.End,
+		Start: woToDateTime(ti.Start),
+		End:   woToDateTime(ti.End),
 	}
 }
